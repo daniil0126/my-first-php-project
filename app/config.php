455948b95861c893612493907828fb49d1aa7a1d
daniil0126 <125200229+daniil0126@users.noreply.github.com>
2024-02-13 00:35:33 +0600

    Create config.php

---
+++ app/config.php
@@ -0,0 +1,19 @@
+<?php
+
+const CONFIG = [
+    'data_file' => APP_PATH . 'data.json',
+    'db' => 'mysql:dbname=new_web_app;host=localhost;port=3306',
+    'db_user' => 'root',
+    'db_password' => '',
+    'admin' => [
+        'admin@admin.com' => '1234'
+    ],
+    'users' => [
+        'user' => '',
+        'password' => '',
+        'email' => '',
+        'phone' => '',
+        'apartment' => '',
+        'floor' => '',
+    ]
+];
