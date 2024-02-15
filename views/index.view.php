46a3d6d9c96ede411c589cb98642f836705171cc
daniil0126 <125200229+daniil0126@users.noreply.github.com>
2024-02-15 20:21:46 +0600

    Create index.view.php

---
+++ views/index.view.php
@@ -0,0 +1,105 @@
+<div id="first" class="first-container">
+    <div class="text">
+        <h1>МЫ ЗАНИМАЕМСЯ ВЫНОСОМ МУСОРА ИЗ ВАШЕЙ КВАРТИРЫ</h1>
+        <h5>Вынесем мусор за вас по низкой цене</h3>
+        <h6 style="color: #544caa;" class="h6">Ниже подробности&#8595</h6>
+    </div>
+    <img class="photo" src="../img/IMG_0683.JPG" alt="">
+</div>
+<div id="second" class="second-container">
+    <h2>Наши услуги</h2>
+    <div class="cards">
+        <div id="card" class="card">
+            <div class="description">
+                <div class="card-name">Тариф: </div>
+                <div class="card-description">В этот тариф входит:</div>
+                <div class="cost">Цена:</div>
+                <input type="submit" class="btn" value="Попробовать">
+            </div>
+        </div>
+        <div id="card" class="card">
+            <div class="description">
+                <div class="card-name">Тариф: </div>
+                <div class="card-description">В этот тариф входит:</div>
+                <div class="cost">Цена:</div>
+                <input type="submit" class="btn" value="Попробовать">
+            </div>
+        </div>
+        <div id="card" class="card">
+            <div class="description">
+                <div class="card-name">Тариф: </div>
+                <div class="card-description">В этот тариф входит:</div>
+                <div class="cost">Цена:</div>
+                <input type="submit" class="btn" value="Попробовать">
+            </div>
+        </div>
+    </div>
+</div>
+<div id="third" class="about-container">
+    <h2>О сервисе</h2>
+    <div class="about-cards">
+        <div class="about-card">
+            <h1 class="about">Как работает сервис?</h1>
+            <p>
+                Вы выставляете свой мусор
+                в назначенное время, в
+                подъезде возле двери, после
+                чего вам больше ничего не нужно делать. Мы приезжаем
+                и забираем мусор, и выбрасываем его!
+            </p>
+        </div>
+        <div class="about-card">
+            <h1 class="about">Что нужно сделать?</h1>
+            <p>
+                Чтобы пользоваться услугами вам нужно: <br>
+                1. Приобрести на них подписку. <br>
+                2. Зарегистрироваться, где нужно будет указать ваш этаж, номер квартиры, и т.д <br>
+                Вы узнаете о наличии подписки в вашем личном кабинете.
+            </p>
+        </div>
+        <div class="about-card">
+            <h1 class="about">Важно знать!</h1>
+            <p>
+                Если ваша подписка истекла, то её необходимо продлить, ведь если окажется,
+                что у вас нет подписки, но вы выставите мусор, мы не станем его забирать.
+            </p>
+        </div>
+    </div>
+</div>
+<footer>
+    <div id="fourth" class="foo-container">
+        <div class="footer-content">
+            <div class="left-part">
+                <p>
+                    Вы также можете связаться <br>
+                    с нами, и задать интересующие <br>
+                    вас вопросы, будем рады вам <br>
+                    помочь!
+                </p>
+            </div>
+            <div class="right-part">
+                <div class="part-content">
+                    <div class="phone-num-column">
+                        <h1>Телефон</h1>
+                        <p>8 (777) 777 77 77</p>
+                    </div>
+                    <div class="socials-column">
+                        <h1>Социальные сети</h1>
+                        <ul class="foot-ul">
+                            <li>
+                                <a class="soc-link" href="">WhatsApp</a>
+                            </li>
+                            <li>
+                                <a class="soc-link" href="">Telegram</a>
+                            </li>
+                            <li>
+                                <a class="soc-link" href="">Instagram</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</footer>
