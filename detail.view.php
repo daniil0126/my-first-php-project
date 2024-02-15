590a40aa10ce0faaef8e1b9f6fb31adf043722e3
daniil0126 <125200229+daniil0126@users.noreply.github.com>
2024-02-15 20:24:11 +0600

    Delete detail.view.php

--- detail.view.php
+++ detail.view.php
@@ -1,8 +1,1 @@
-<div class="first-container">
-    <div class="text">
-        <h1>МЫ ЗАНИМАЕМСЯ ВЫНОСОМ МУСОРА ИЗ ВАШЕЙ КВАРТИРЫ</h1>
-        <h5>Вынесем мусор за вас по низкой цене</h3>
-            <input type="submit" class="btn" value="Узнать">
-    </div>
-    <img class="photo" src="../img/IMG_0683.JPG" alt="">
-</div>
+
